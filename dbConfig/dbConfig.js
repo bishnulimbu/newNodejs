55b9129d2fbc4a7dbaf97cdc9235ca7a6d93767b
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-11 08:19:48 +0545

    database config file

---
+++ dbConfig/dbConfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  username: 'ram',
+  host: 'localhost',
+  bd: 'okcrud',
+  password: '',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 0,
+    acquire: 1000,
+  }
+}
