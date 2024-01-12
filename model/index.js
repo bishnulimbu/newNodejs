0fb03b23e4f707493db2acf46141c959a09c2e26
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-12 10:34:13 +0545

    dsafa

--- model/index.js
+++ model/index.js
@@ -1,21 +1,3 @@
-// const dbConfig = require('../dbConfig/dbConfig.js');
-// const { Sequelize } = require('sequelize');
-// const sequelize = new sequelize{
-// dbConfig.db,
-// dbConfig.username,
-// dbConfig.password,
-//   {
-//  host: bdConfig.host,
-// dialect: dbConfig.dialect;
-//   pool: {
-//   max: dbConfig.pool.max,
-//   min: dbConfig.pool.min,
-//   accurate: dbConfig.pool.accurate,
-//   idle: dbConfig.pool.idle,
-// },
-// }
-// };
-
 const dbConfig = require('../dbConfig/dbConfig.js');
 const { Sequelize } = require('sequelize');
 
@@ -26,7 +8,7 @@ const sequelize = new Sequelize(dbConfig.db,
     host: dbConfig.host,
     dialect: dbConfig.dialect,
     pool: {
-      max: dbConfig.pool.max,
+
       min: dbConfig.pool.min,
       acquire: dbConfig.pool.accurate, // Change 'accurate' to 'acquire'
       idle: dbConfig.pool.idle,
@@ -40,29 +22,10 @@ sequelize
   })
   .catch((err) => {
     console.log(err);
-
   });
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-module.exports = db;
 
-db.blog = require("./blog.js")(
-  sequelize, Sequelize
-)
-// const dbconfig = require("../dbconfig/dbconfig.js");
-// const sequelize = require("sequelize");
-//
-// const sequelize = new sequelize(
-//   dbconfig.db,
-//   dbconfig.username,
-//   dbconfig.password,
-//   {
-//     host: dbconfig.host,
-//     dialect: dbconfig.dialect,
-//     pool: {
-//       max: dbconfig.pool.max,
-//       min: dbconfig.pool.min,
-//       accurate: dbconfig.pool.accurate,
-//       idle: dbconfig.pool.idle,
-//     },
+db.blogs = require("./../model/blog.js")(sequelize, Sequelize);
+module.exports = db;
