6c7a37b8dca7ebf5782d215ae763c1a002c1d0c6
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-12 10:34:17 +0545

    dsf

---
+++ routes/blogs.js
@@ -0,0 +1,10 @@
+const { createBlog, deleteBlogsById, getBlogs, getBlogsById } = require("../controllers/blogController");
+
+const router = require("express").Router();
+
+router.post("/create", createBlog);
+router.get("/get", getBlogs);
+router.get("/get/:id", getBlogsById);
+router.get("/delete/:id", deleteBlogsById);
+
+module.exports = router;
