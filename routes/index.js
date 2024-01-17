5909d83a4a1108050a96f28a36117dd14920271a
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-17 18:28:39 +0545

    dafsd

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,22 @@
+// // const express = require("express");
+// // const { testFunction } = require("./controllers/testController");
+// // const app = express();
+// //
+// // const db = require("./model/index.js");
+// // db.sequelize.sync({ force: false });
+// //
+// //
+// // app.use(express.json())
+// // app.use(express.urlencoded({ extended: true }))
+// //
+// //
+// // const createRoutes = require("./routes/blogs.js");
+// //
+// // app.use("/api", createRoutes);
+// // let PORT = 5000;
+// // app.listen(PORT, () => {
+// //   console.log(`server is started in ${PORT}`);
+// // });
 // const express = require("express");
 // const { testFunction } = require("./controllers/testController");
 // const app = express();
@@ -7,32 +26,14 @@
 //
 //
 // app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
+// // app.use(express.urlencoded({ extended: true }))
 //
 //
 // const createRoutes = require("./routes/blogs.js");
+// const createRoutes = require("./routes/auth.js");
 //
 // app.use("/api", createRoutes);
 // let PORT = 5000;
 // app.listen(PORT, () => {
 //   console.log(`server is started in ${PORT}`);
 // });
-const express = require("express");
-const { testFunction } = require("./controllers/testController");
-const app = express();
-
-const db = require("./model/index.js");
-db.sequelize.sync({ force: false });
-
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-
-const createRoutes = require("./routes/blogs.js");
-
-app.use("/api", createRoutes);
-let PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`server is started in ${PORT}`);
-});
